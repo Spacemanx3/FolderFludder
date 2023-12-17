17ffd69d1cf8755fed00b99256910e8f018d17ce
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-16 19:34:49 -0500

    Update dir.sh

--- dir.sh
+++ dir.sh
@@ -4,17 +4,29 @@
 current_directory=$(pwd)
 echo "Current directory: $current_directory"
 
-# Search for specific directories and print the results
+# Search for specific directories in the Documents and Pictures directories
 search_directories=("jpeg" "pwd" "root")
+documents_directory="/mnt/chromeos/MyFiles/Documents"
+pictures_directory="/mnt/chromeos/MyFiles/Pictures"
 
-for dir_name in "${search_directories[@]}"; do
-    echo -e "\nSearch results for directory '$dir_name':"
-    
-    found_directories=$(find / -type d -name "$dir_name" -print 2>/dev/null)
+# Function to search and print results
+search_and_print() {
+    local directory=$1
+    for dir_name in "${search_directories[@]}"; do
+        echo -e "\nSearch results for directory '$dir_name' in $directory:"
+        
+        found_directories=$(find "$directory" -type d -name "$dir_name" -print 2>/dev/null)
 
-    if [ -z "$found_directories" ]; then
-        echo "Directory not found."
-    else
-        echo "$found_directories"
-    fi
-done
+        if [ -z "$found_directories" ]; then
+            echo "Directory not found."
+        else
+            echo "$found_directories"
+        fi
+    done
+}
+
+# Search in Documents directory
+search_and_print "$documents_directory"
+
+# Search in Pictures directory
+search_and_print "$pictures_directory"
