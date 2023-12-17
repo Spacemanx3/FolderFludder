015c623138e99ead2533382830c798157cd100bd
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-16 19:21:44 -0500

    Create dir.sh

---
+++ dir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Print the current directory
+current_directory=$(pwd)
+echo "Current directory: $current_directory"
+
+# Search for specific directories and print the results
+search_directories=("jpeg" "pwd" "root")
+
+for dir_name in "${search_directories[@]}"; do
+    echo -e "\nSearch results for directory '$dir_name':"
+    
+    found_directories=$(find . -type d -name "$dir_name" -print)
+
+    if [ -z "$found_directories" ]; then
+        echo "Directory not found."
+    else
+        echo "$found_directories"
+    fi
+done
