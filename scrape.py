ff2c1a92bd24e10128ed8bca4b5d5ecc7db75f3e
Spacemanx3 <127046307+Spacemanx3@users.noreply.github.com>
2023-12-24 01:46:45 -0500

    Update scrape.sh

--- scrape.py
+++ scrape.py
@@ -1,20 +1,35 @@
-import os
+#!/bin/bash
 
-def search_files_and_write(start_path='/', output_file='sysFold.txt'):
-    with open(output_file, 'w') as file:
-        for root, dirs, files in os.walk(start_path):
-            file.write(f"\nDirectory: {root}\n")
-            for file_name in files:
-                file_path = os.path.join(root, file_name)
-                file.write(f"{file_path}\n")
+# Check if ngrok is installed
+if ! command -v ngrok &> /dev/null; then
+    echo "ngrok is not installed. Please install it first."
+    exit 1
+fi
 
-def print_directory_contents(start_path='/'):
-    for root, dirs, files in os.walk(start_path):
-        print(f"\nDirectory: {root}")
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            print(file_path)
+# Your local server address
+LOCAL_SERVER_ADDRESS="http://localhost:8080"
 
-if __name__ == "__main__":
-    search_files_and_write()
-    print_directory_contents()
+# Ask for the remote user's information
+read -p "Enter the remote user's IP address: " REMOTE_IP
+read -p "Enter the remote user's MAC address: " REMOTE_MAC
+read -p "Enter the remote user's network IP address: " REMOTE_NETWORK_IP
+
+# Start ngrok to expose the local server
+ngrok http 8080 &
+
+# Wait for ngrok to generate a public URL
+sleep 5
+
+# Get the public URL from ngrok
+PUBLIC_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+# Craft the hello message
+HELLO_MESSAGE="Hello from $LOCAL_SERVER_ADDRESS through ngrok! Public URL: $PUBLIC_URL"
+
+# Send the hello message to the remote user
+echo "$HELLO_MESSAGE" | nc -w 1 $REMOTE_IP 12345
+echo "$HELLO_MESSAGE" | nc -w 1 $REMOTE_MAC 12345
+echo "$HELLO_MESSAGE" | nc -w 1 $REMOTE_NETWORK_IP 12345
+
+# Stop ngrok
+pkill ngrok
